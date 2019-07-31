084700c80023d4fb291bee36f41cb99f23f7dffa
sehe <sgheeren@gmail.com>
2019-07-31 22:51:20 +0200

    Fix the context_type in config.hpp

--- x3/employee/config.hpp
+++ x3/employee/config.hpp
@@ -1,6 +1,7 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
+#include <boost/spirit/home/x3/support/ast/position_tagged.hpp>
 #include <boost/spirit/home/x3.hpp>
 #include <string>
 
@@ -11,7 +12,17 @@ namespace parser
 namespace x3 = boost::spirit::x3;
 
 using iterator_type = std::string::const_iterator;
-using context_type = x3::phrase_parse_context<x3::ascii::space_type>::type;
+
+struct position_cache_tag;
+using position_cache = boost::spirit::x3::position_cache<std::vector<iterator_type>>;
+
+using simple_context_type = x3::phrase_parse_context<x3::ascii::space_type>::type;
+
+using context_type = boost::spirit::x3::context<
+    client::parser::position_cache_tag,
+    std::reference_wrapper<position_cache>, 
+    simple_context_type
+>;
 } // namespace parser
 } // namespace client
 
